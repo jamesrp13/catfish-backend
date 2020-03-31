eb36dfe41625bf202cf3ab317646001863308d3b
John Afolabi <johnafolabi.da@gmail.com>
2020-03-31 23:31:35 +0100

    Configure knex

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true
+  }
+};
