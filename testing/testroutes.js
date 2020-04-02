8690d1ce6e0aaf16cff8f103ed708c9a72f65d37
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-04-01 22:03:39 -0700

    Created more endpoints for Profiles, Posts, and Instances

--- testing/testroutes.js
+++ testing/testroutes.js
@@ -8,6 +8,7 @@ router.use(bodyParser.json());
 
 
 //// Variables for Temp Storage
+// #region Test DB
 // Counter to assign id to new users
 let numUsers  = 0;
 // Array of users
@@ -28,24 +29,26 @@ let tempPosts = [];
 let numComms  = 0;
 // Array of comments
 let tempComms = [];
+// #endregion Test DB
 
-//// Profiles
-// #region GET Profiles
-router.get("/001/profile", (req, res) => {      //Show all profiles
+//// Users
+// #region GET Users
+router.get("/user", (req, res) => {      //Show all users
     res.json(testUsers);
 })
-router.get("/001/profile/:id", (req, res) => {  //Get profile based on id
+router.get("/user/:id", (req, res) => {  //Get user based on id
     res.json(testUsers[req.params.id]);
 })
-// #endregion GET Profiles
-// #region POST Profile
-router.post("/001/profile", (req, res) => {     //New profile
-    //Add profile to database
+// #endregion GET Users
+// #region POST Users
+router.post("/user", (req, res) => {     //New user
+    //Add user to database
     testUsers.push({
-    user  : req.body.user,
-    pass  : req.body.pass,
-    email : req.body.email,
-    id    : numUsers++
+    user_id   : numUsers++,
+    firstname : req.body.fname,
+    lastname  : req.body.lname,
+    password  : req.body.pass,
+    email     : req.body.email
     });
     //Get a login token??
     testUsers[numUsers - 1].token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
@@ -55,7 +58,7 @@ router.post("/001/profile", (req, res) => {     //New profile
         token : testUsers[numUsers - 1].token
     });
 })
-// #endregion POST Profile
+// #endregion POST Users
 
 //// Instances
 // #region GET Instances
@@ -80,5 +83,76 @@ router.post('/instances', (req, res) => {
         id : numInsts - 1
     })
 })
+// #endregion POST Instances
+
+//// Profiles
+// #region GET Profiles
+router.get("/game", (req, res) => {
+    res.json(tempProfs);
+})
+router.get("/game/:id", (req, res) => {  
+    res.json(tempProfs[req.params.id]);
+})
+// #endregion GET Profiles
+// #region POST Profiles
+router.post('/game', (req, res) => {
+    tempProfs.push({
+        profile_id   : numProfs++,
+        display_name : req.body.name,
+        about        : req.body.bio,
+        dob          : req.body.dob,
+        pic_url      : req.body.pic,
+        user_id      : req.body.user,
+        instance_id  : req.body.inst
+    });
+    res.json({
+        id: numProfs - 1
+    })
+})
+// #endregion POST Profiles
+
+//// Posts
+// #region GET Posts
+router.get("/game/:inst/post", (req, res) => {
+    //Get all posts from a certain game instance
+    let tempRes = tempPosts.filter(x => {
+        return x.instance_id == req.params.inst;
+    })
+    //Include pagination
+    
+    res.json(tempRes);
+})
+router.get("/game/:inst/post/:id", (req, res) => {
+    //Get all posts from a certain game instance
+    let tempRes = tempPosts.filter(x => {
+        return x.instance_id == req.params.inst &&
+               x.profile_id  == req.params.id;
+    })
+    //Include pagination
+    
+    res.json(tempRes);
+})
+// #endregion GET Posts
+// #region POST Posts
+router.post("/game/:inst/post", (req, res) => {
+    tempPosts.push({
+        post_id        : numPosts++,
+        post_content   : req.body.content,
+        post_image_url : req.body.img,
+        post_timestamp : req.body.time,
+        instance_id    : req.body.inst,
+        profile_id     : req.body.prof
+    });
+    res.json({
+        id : numPosts - 1,
+    });
+})
+// #endregion POST Posts
+
+//// Comments
+// #region GET Comments
+// #endregion GET Comments
+// #region POST Comments
+// #endregion POST Comments
 
 module.exports = router;
