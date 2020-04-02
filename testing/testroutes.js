992e1a19d00b8364d52c3f001347370a8832d1e8
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-04-01 20:29:20 -0700

    added endpoints for GET and POST of instance table

--- testing/testroutes.js
+++ testing/testroutes.js
@@ -38,7 +38,6 @@ router.get("/001/profile/:id", (req, res) => {  //Get profile based on id
     res.json(testUsers[req.params.id]);
 })
 // #endregion GET Profiles
-
 // #region POST Profile
 router.post("/001/profile", (req, res) => {     //New profile
     //Add profile to database
@@ -52,7 +51,7 @@ router.post("/001/profile", (req, res) => {     //New profile
     testUsers[numUsers - 1].token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     //Send back userid and login token
     res.json({
-        id    : testUsers[numUsers - 1].id,
+        id    : numUsers - 1,
         token : testUsers[numUsers - 1].token
     });
 })
@@ -60,5 +59,26 @@ router.post("/001/profile", (req, res) => {     //New profile
 
 //// Instances
 // #region GET Instances
+router.get('/instances', (req, res) => {
+    res.json(testInsts);
+});
+router.get('/instances/:id', (req, res) => {
+    res.json(testInsts[req.params.id]);
+});
+// #endregion GET Instances
+// #region POST Instances
+router.post('/instances', (req, res) => {
+    // Store the new instance in db and assign a n index
+    testInsts.push({
+        title      : req.body.title,
+        start_time : req.body.timestamp,
+        duration   : req.body.duration,
+        id         : numInsts++
+    });
+    // Send back instance id
+    res.json({
+        id : numInsts - 1
+    })
+})
 
 module.exports = router;
